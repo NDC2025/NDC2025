10599a1d55f9a46cfd57b152cf92acd22c3e6172
NDC2025 <ndcstex2025@gmail.com>
2025-04-02 15:37:13 +0200

    Update controle.mjs

--- controle.mjs
+++ controle.mjs
@@ -17,10 +17,11 @@ const db = getFirestore()
 
 const querySnapshot = await getDocs(collection(db, "Equipe"));
 querySnapshot.forEach((doc) => {
-  let liste = document.createElement("li")
-  let equipe = document.createElement("button")
-  equipe.innertHTML = doc.id
-  liste.appendChild(equipe)
+  let liste = document.createElement("li");
+  let equipe = document.createElement("button");
+  equipe.innertHTML = doc.id;
+  liste.appendChild(equipe);
+  document.getElementById("liste-equipe").appendChild(liste);
 });
 
 
