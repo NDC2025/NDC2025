a2a3c16834ccb38556dd55eaec22427210be90f8
NDC2025 <ndcstex2025@gmail.com>
2025-04-02 22:42:52 +0200

    Update creercompte.mjs

--- creercompte.mjs
+++ creercompte.mjs
@@ -1,1 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhrYj90-pt7fMEQjzHhNzoBm8AsfD2njI",
+  authDomain: "ndc2025-6a79d.firebaseapp.com",
+  projectId: "ndc2025-6a79d",
+  storageBucket: "ndc2025-6a79d.firebasestorage.app",
+  messagingSenderId: "153804251895",
+  appId: "1:153804251895:web:573edb252ef96d4bcda9f2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+document.getElementById("btncreer").addEventListener("click", function () {
+  const nom = document.getElementById("nom").value
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+  const classe = document.getElementById("classe").value
+  createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+    const user = userCredential.user
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorCode, errorMessage)
+  })
+
+
+})
