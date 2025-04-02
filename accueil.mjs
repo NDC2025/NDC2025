6f8339f75f1fb37c1af49e7e70c845509912e2b7
NDC2025 <ndcstex2025@gmail.com>
2025-04-02 14:32:59 +0200

    Update accueil.mjs

--- accueil.mjs
+++ accueil.mjs
@@ -1,1 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAhrYj90-pt7fMEQjzHhNzoBm8AsfD2njI",
+  authDomain: "ndc2025-6a79d.firebaseapp.com",
+  projectId: "ndc2025-6a79d",
+  storageBucket: "ndc2025-6a79d.firebasestorage.app",
+  messagingSenderId: "153804251895",
+  appId: "1:153804251895:web:573edb252ef96d4bcda9f2"
+};
+
+const app = initializeApp(firebaseConfig);
