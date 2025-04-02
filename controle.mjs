6accc70901a91366a76d2efb8654ee36d79a8ec6
maxdelacotte <123182019+maxdelacotte@users.noreply.github.com>
2025-04-02 15:01:56 +0200

    Update controle.mjs

--- controle.mjs
+++ controle.mjs
@@ -1,1 +1,24 @@
-import 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhrYj90-pt7fMEQjzHhNzoBm8AsfD2njI",
+  authDomain: "ndc2025-6a79d.firebaseapp.com",
+  projectId: "ndc2025-6a79d",
+  storageBucket: "ndc2025-6a79d.firebasestorage.app",
+  messagingSenderId: "153804251895",
+  appId: "1:153804251895:web:573edb252ef96d4bcda9f2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+const ft = getFirestore()
+
+equipe = await getDocs(collection(ft, "Equipe"));
+
+getDocs(collection(db, "cities")).forEach((doc) => {
+  console.log(doc.id, " => ", doc.data());
+});
+
