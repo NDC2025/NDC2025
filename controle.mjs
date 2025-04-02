a5c3a7c154de909125dd2e9328e12b354868c3a2
maxdelacotte <123182019+maxdelacotte@users.noreply.github.com>
2025-04-02 19:37:01 +0200

    Update controle.mjs

--- controle.mjs
+++ controle.mjs
@@ -17,11 +17,12 @@ const db = getFirestore()
 
 const querySnapshot = await getDocs(collection(db, "Equipe"));
 querySnapshot.forEach((doc) => {
-  let liste = document.createElement("li");
-  let equipe = document.createElement("p");
-  equipe.innertHTML = doc.id;
-  liste.appendChild(equipe);
-  document.getElementById("liste-equipe").appendChild(liste);
+  //let liste = document.createElement("li");
+  //let equipe = document.createElement("p");
+  //equipe.innertHTML = doc.id;
+  //liste.appendChild(equipe);
+  //document.getElementById("liste-equipe").appendChild(liste);
+  document.getElementById("liste-equipe").appendChild(document.creatElement("li").appendChild(document.creatEkement("p").innertHTML = doc.id))
 });
 
 
