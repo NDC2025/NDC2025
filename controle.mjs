74c6662b81740beb8644bcf213aa11189e1b5ecc
NDC2025 <ndcstex2025@gmail.com>
2025-04-02 19:55:07 +0200

    Update controle.mjs

--- controle.mjs
+++ controle.mjs
@@ -20,7 +20,7 @@ querySnapshot.forEach((doc) => {
   let liste = document.createElement("li");
   let equipe = document.createElement("p");
   equipe.innertHTML = doc.id;
-  console.log(doc.id);
+  console.log(doc.id, equipe);
   liste.appendChild(equipe);
   document.getElementById("liste-equipe").appendChild(liste);
   //document.getElementById("liste-equipe").appendChild(document.creatElement("li").appendChild(document.creatEkement("p").innertHTML = doc.id));
